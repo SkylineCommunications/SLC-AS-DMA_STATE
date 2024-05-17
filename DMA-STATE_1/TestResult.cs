692799b2ce3bea47e589eb891fb3bc6180583573
Sergio Suárez <sergio.suarez@skyline.be>
2024-05-17 12:37:43 -0500

    Fist version of the script
    Script returns an object containing dma name and dma state

---
+++ DMA-STATE_1/TestResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMA_STATE_1
+{
+    public class TestResult
+	{
+		public string ParameterName { get; set; }
+
+		public string DisplayName { get; set; }
+
+		public string ElementName { get; set; }
+
+		public string DmaName { get; set; }
+
+		public string ReceivedValue { get; set; }
+	}
+}
